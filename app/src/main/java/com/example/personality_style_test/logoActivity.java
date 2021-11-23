18a98ff32b597f7522f3f1800ad8d9e4c7f58ccc
subinkim26 <dpaptlove@gmail.com>
2021-11-23 17:34:22 +0900

    logo clickListener
    logo clickListener

--- app/src/main/java/com/example/personality_style_test/logoActivity.java
+++ app/src/main/java/com/example/personality_style_test/logoActivity.java
@@ -2,7 +2,11 @@ package com.example.personality_style_test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 
 public class logoActivity extends AppCompatActivity {
 
@@ -10,5 +14,35 @@ public class logoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
+
+
+        ImageView imageView = findViewById(R.id.imageView);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+
+
+            }
+        });
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class); //화면 전환
+                startActivity(intent);
+                finish();
+            }
+        }, 2000); //딜레이 타임 조절
+
     }
+
+
+
+
 }
