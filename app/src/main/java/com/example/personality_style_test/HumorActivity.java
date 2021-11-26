85848777ed25d61daa10dea082cfc5e757542a2a
minm063 <jangmin063@gmail.com>
2021-11-26 16:54:19 +0900

    description update

--- app/src/main/java/com/example/personality_style_test/HumorActivity.java
+++ app/src/main/java/com/example/personality_style_test/HumorActivity.java
@@ -20,7 +20,9 @@ public class HumorActivity extends AppCompatActivity {
 
         ListView humor_list = (ListView)findViewById(R.id.humor_list);
         HumorAdapter adapter = new HumorAdapter();
-        adapter.addItem(new HumorItem("호불호테스트", "호불호찾기 테스트","소요시간 : 5분", R.drawable.logo));
+        adapter.addItem(new HumorItem("음식 호불호테스트", "당신의 음식 호불호 취향을 알아보세요. 내가 어디까지 먹을 수 있을지 테스트 해보세요.","소요시간 : 2분 내외", R.drawable.foodtest));
+        adapter.addItem(new HumorItem("내가 산타라면?", "크리스마스를 맞아 아이들에게 선물을 나눠주려고 합니다. 과연 나는 어떤 산타일까요?", "소요시간 : 3분 내외", R.drawable.santatest));
+        adapter.addItem(new HumorItem("크리스마스 케이크 테스트", "이번 크리스마스에는 어떤 케이크가 나와 어울릴까? 케이크에 대해 고민을 하고 있는 당신을 위해 테스트를 통해 케이크를 골라드립니다.", "소요시간 : 3분 내외", R.drawable.caketest));
         humor_list.setAdapter(adapter);
 
     }
