e5d5841e65a32ef3695282fa29549ef7e42003eb
subinkim26 <dpaptlove@gmail.com>
2021-12-03 16:35:12 +0900

    bankingtest test

--- app/src/main/java/com/example/personality_style_test/bankingtest/bankingtest_6_Activity.java
+++ app/src/main/java/com/example/personality_style_test/bankingtest/bankingtest_6_Activity.java
@@ -2,7 +2,9 @@ package com.example.personality_style_test.bankingtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.personality_style_test.R;
@@ -17,5 +19,77 @@ public class bankingtest_6_Activity extends AppCompatActivity {
         Button bankingtest_6_an1 = (Button) findViewById(R.id.bankingtest_5_an1);
         Button bankingtest_6_an2 = (Button) findViewById(R.id.bankingtest_5_an2);
         Button bankingtest_6_an3 = (Button) findViewById(R.id.bankingtest_5_an3);
+
+        bankingtest_6_an1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent5 = getIntent();
+                Bundle bundle = intent5.getExtras();
+
+                String bankingtest_result1 = bundle.getString("bankingtest_result1");
+                String bankingtest_result2 = bundle.getString("bankingtest_result2");
+                String bankingtest_result3 = bundle.getString("bankingtest_result3");
+
+                if(bankingtest_result3 == "result3"){
+                    Intent intent6 = new Intent(getBaseContext(), bankingtest_result3_Activity.class);
+                    //Toast.makeText(getApplicationContext(), bankingtest_result1,Toast.LENGTH_SHORT).show();
+                    startActivity(intent6);
+                }
+                else if(bankingtest_result1 == "result1"){
+                    Intent intent6 = new Intent(getBaseContext(), bankingtest_result1_Activity.class);
+                    startActivity(intent6);
+                }
+                else{
+                    Intent intent6 = new Intent(getBaseContext(), bankingtest_result2_Activity.class);
+                    startActivity(intent6);
+                }
+
+            }
+        });
+
+        bankingtest_6_an2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent5 = getIntent();
+                Bundle bundle = intent5.getExtras();
+
+                String bankingtest_result1 = bundle.getString("bankingtest_result1");
+                String bankingtest_result2 = bundle.getString("bankingtest_result2");
+                String bankingtest_result3 = bundle.getString("bankingtest_result3");
+
+                if(bankingtest_result3 == "result3"){
+                    Intent intent6 = new Intent(getBaseContext(), bankingtest_result3_Activity.class);
+                    //Toast.makeText(getApplicationContext(), bankingtest_result1,Toast.LENGTH_SHORT).show();
+                    startActivity(intent6);
+                }
+                else if(bankingtest_result1 == "result1"){
+                    Intent intent6 = new Intent(getBaseContext(), bankingtest_result1_Activity.class);
+                    startActivity(intent6);
+                }
+                else{
+                    Intent intent6 = new Intent(getBaseContext(), bankingtest_result2_Activity.class);
+                    startActivity(intent6);
+                }
+            }
+        });
+
+        bankingtest_6_an3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                Intent intent5 = getIntent();
+//                Bundle bundle = intent5.getExtras();
+
+//                String bankingtest_result1 = bundle.getString("bankingtest_result1");
+//                String bankingtest_result2 = bundle.getString("bankingtest_result2");
+//                String bankingtest_result3 = "result3";
+
+                Intent intent6 = new Intent(getBaseContext(), bankingtest_result3_Activity.class);
+                //Toast.makeText(getApplicationContext(), bankingtest_result1,Toast.LENGTH_SHORT).show();
+                startActivity(intent6);
+            }
+        });
     }
 }
