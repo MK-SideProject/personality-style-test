a052b7d7c1c9fd3d56cb015f88809bd494977b1c
minm063 <jangmin063@gmail.com>
2021-12-04 17:16:56 +0900

    santatest upload

--- app/src/main/java/com/example/personality_style_test/santatest/santatest_1_Activity.java
+++ app/src/main/java/com/example/personality_style_test/santatest/santatest_1_Activity.java
@@ -2,10 +2,13 @@ package com.example.personality_style_test.santatest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.personality_style_test.R;
+import com.example.personality_style_test.bankingtest.bankingtest_2_Activity;
 
 public class santatest_1_Activity extends AppCompatActivity {
 
@@ -16,6 +19,30 @@ public class santatest_1_Activity extends AppCompatActivity {
 
         Button santatest_1_an1 = (Button) findViewById(R.id.santatest_1_an1);
         Button santatest_1_an2 = (Button) findViewById(R.id.santatest_1_an2);
+
+        santatest_1_an1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String santatest_result3 = "result3";
+
+                Intent intent = new Intent(getBaseContext(), santatest_2_Activity.class);
+                intent.putExtra("santatest_result3", santatest_result3);
+
+                startActivity(intent);
+            }
+        });
+
+        santatest_1_an2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String santatest_result4 = "result4";
+
+                Intent intent = new Intent(getBaseContext(), santatest_2_Activity.class);
+                intent.putExtra("santatest_result4", santatest_result4);
+
+                startActivity(intent);
+            }
+        });
 
     }
 }
