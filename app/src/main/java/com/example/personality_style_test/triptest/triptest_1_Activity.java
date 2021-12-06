291bdf6f25c3a951a8dccfc1da4853461d3a09a9
subinkim26 <dpaptlove@gmail.com>
2021-12-06 16:36:27 +0900

    triptest update

--- app/src/main/java/com/example/personality_style_test/triptest/triptest_1_Activity.java
+++ app/src/main/java/com/example/personality_style_test/triptest/triptest_1_Activity.java
@@ -2,10 +2,13 @@ package com.example.personality_style_test.triptest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.personality_style_test.R;
+import com.example.personality_style_test.santatest.santatest_2_Activity;
 
 public class triptest_1_Activity extends AppCompatActivity {
 
@@ -17,6 +20,45 @@ public class triptest_1_Activity extends AppCompatActivity {
         Button triptest_1_an1 = (Button) findViewById(R.id.triptest_1_an1);
         Button triptest_1_an2 = (Button) findViewById(R.id.triptest_1_an2);
         Button triptest_1_an3 = (Button) findViewById(R.id.triptest_1_an3);
+
+        triptest_1_an1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+
+                String alone_x = "alone_x";
+
+                Intent intent = new Intent(getBaseContext(), triptest_2_Activity.class);
+                intent.putExtra("alone_x", alone_x);
+                startActivity(intent);
+            }
+        });
+
+        triptest_1_an2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+
+                String alone_x = "alone_x";
+
+                Intent intent = new Intent(getBaseContext(), triptest_2_Activity.class);
+                intent.putExtra("alone_x", alone_x);
+                startActivity(intent);
+            }
+        });
+
+        triptest_1_an3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+
+                String alone_o = "alone_o";
+
+                Intent intent = new Intent(getBaseContext(), triptest_2_Activity.class);
+                intent.putExtra("alone_o", alone_o);
+                startActivity(intent);
+            }
+        });
 
     }
 }
