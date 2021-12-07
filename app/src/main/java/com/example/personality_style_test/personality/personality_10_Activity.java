faa15a7be67b280ac273e04d427c1c5bb5fa5e7d
subinkim26 <dpaptlove@gmail.com>
2021-12-07 18:07:27 +0900

    personality activitiy

---
+++ app/src/main/java/com/example/personality_style_test/personality/personality_10_Activity.java
@@ -0,0 +1,14 @@
+package com.example.personality_style_test.personality;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class personality_10_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personality10);
+    }
+}
---
+++ app/src/main/java/com/example/personality_style_test/personality/personality_31_Activity.java
@@ -0,0 +1,16 @@
+package com.example.personality_style_test.personality;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.personality_style_test.R;
+
+public class personality_31_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personality31);
+    }
+}
