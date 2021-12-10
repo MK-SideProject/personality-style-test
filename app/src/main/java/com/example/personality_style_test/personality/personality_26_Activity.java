5a22e42a79f375e827f6341ad9613c16ad0e9cbe
minm063 <jangmin063@gmail.com>
2021-12-10 16:56:31 +0900

    intent upload

--- app/src/main/java/com/example/personality_style_test/personality/personality_26_Activity.java
+++ app/src/main/java/com/example/personality_style_test/personality/personality_26_Activity.java
@@ -2,7 +2,9 @@ package com.example.personality_style_test.personality;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.personality_style_test.R;
@@ -17,5 +19,68 @@ public class personality_26_Activity extends AppCompatActivity {
         Button personality_26_yes = (Button) findViewById(R.id.personality_26_yes);
         Button personality_26_no = (Button) findViewById(R.id.personality_26_no);
 
+        personality_26_yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent25 = getIntent();
+                Bundle bundle = intent25.getExtras();
+
+                int e = bundle.getInt("e");
+                int i = bundle.getInt("i");
+                int n = bundle.getInt("n");
+                int s = bundle.getInt("s");
+                int f = bundle.getInt("f");
+                int t = bundle.getInt("t");
+                int p = bundle.getInt("p");
+                int j = bundle.getInt("j");
+
+                s+=1;
+
+                Intent intent26 = new Intent(getBaseContext(), personality_27_Activity.class);
+                intent26.putExtra("e", e);
+                intent26.putExtra("i", i);
+                intent26.putExtra("n", n);
+                intent26.putExtra("s", s);
+                intent26.putExtra("f", f);
+                intent26.putExtra("t", t);
+                intent26.putExtra("p", p);
+                intent26.putExtra("j", j);
+
+                startActivity(intent26);
+
+            }
+        });
+
+        personality_26_no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent25 = getIntent();
+                Bundle bundle = intent25.getExtras();
+
+                int e = bundle.getInt("e");
+                int i = bundle.getInt("i");
+                int n = bundle.getInt("n");
+                int s = bundle.getInt("s");
+                int f = bundle.getInt("f");
+                int t = bundle.getInt("t");
+                int p = bundle.getInt("p");
+                int j = bundle.getInt("j");
+
+                n+=1;
+
+                Intent intent26 = new Intent(getBaseContext(), personality_27_Activity.class);
+                intent26.putExtra("e", e);
+                intent26.putExtra("i", i);
+                intent26.putExtra("n", n);
+                intent26.putExtra("s", s);
+                intent26.putExtra("f", f);
+                intent26.putExtra("t", t);
+                intent26.putExtra("p", p);
+                intent26.putExtra("j", j);
+
+                startActivity(intent26);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/personality_style_test/personality/personality_40_Activity.java
+++ app/src/main/java/com/example/personality_style_test/personality/personality_40_Activity.java
@@ -2,7 +2,9 @@ package com.example.personality_style_test.personality;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.personality_style_test.R;
@@ -16,6 +18,70 @@ public class personality_40_Activity extends AppCompatActivity {
 
         Button personality_40_yes = (Button) findViewById(R.id.personality_40_yes);
         Button personality_40_no = (Button) findViewById(R.id.personality_40_no);
+
+        personality_40_yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent39 = getIntent();
+                Bundle bundle = intent39.getExtras();
+
+                int e = bundle.getInt("e");
+                int i = bundle.getInt("i");
+                int n = bundle.getInt("n");
+                int s = bundle.getInt("s");
+                int f = bundle.getInt("f");
+                int t = bundle.getInt("t");
+                int p = bundle.getInt("p");
+                int j = bundle.getInt("j");
+
+                n+=1;
+
+                Intent intent40 = new Intent(getBaseContext(), personality_40_Activity.class);
+                intent40.putExtra("e", e);
+                intent40.putExtra("i", i);
+                intent40.putExtra("n", n);
+                intent40.putExtra("s", s);
+                intent40.putExtra("f", f);
+                intent40.putExtra("t", t);
+                intent40.putExtra("p", p);
+                intent40.putExtra("j", j);
+
+                startActivity(intent40);
+
+            }
+        });
+
+        personality_40_no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent39 = getIntent();
+                Bundle bundle = intent39.getExtras();
+
+                int e = bundle.getInt("e");
+                int i = bundle.getInt("i");
+                int n = bundle.getInt("n");
+                int s = bundle.getInt("s");
+                int f = bundle.getInt("f");
+                int t = bundle.getInt("t");
+                int p = bundle.getInt("p");
+                int j = bundle.getInt("j");
+
+                s+=1;
+
+                Intent intent40 = new Intent(getBaseContext(), personality_40_Activity.class);
+                intent40.putExtra("e", e);
+                intent40.putExtra("i", i);
+                intent40.putExtra("n", n);
+                intent40.putExtra("s", s);
+                intent40.putExtra("f", f);
+                intent40.putExtra("t", t);
+                intent40.putExtra("p", p);
+                intent40.putExtra("j", j);
+
+                startActivity(intent40);
+
+            }
+        });
 
     }
 }
