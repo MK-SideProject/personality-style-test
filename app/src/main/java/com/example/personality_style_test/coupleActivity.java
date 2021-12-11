30dee698250d973bc6f565e1bc17814b772dc67f
subinkim26 <dpaptlove@gmail.com>
2021-12-11 18:07:03 +0900

    coupleActivity update

--- app/src/main/java/com/example/personality_style_test/coupleActivity.java
+++ app/src/main/java/com/example/personality_style_test/coupleActivity.java
@@ -2,11 +2,13 @@ package com.example.personality_style_test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -27,8 +29,24 @@ public class coupleActivity extends AppCompatActivity {
         couple_input_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String mbti = couple_input_text.getText().toString();
+                mbti  = mbti.toUpperCase();
 
-                
+                if (mbti.length() == 0 ) {
+                        //공백일 때 처리할 내용
+                    Toast.makeText(getApplicationContext(), "값을 입력해서 눌러주세요.",Toast.LENGTH_SHORT).show();
+
+                }
+                else if(mbti=="ENTJ" ) {
+                    //텍스트 뷰 전환해주기
+                    couple_mbti_result_text.setText("ENTJ와 어울리는 궁합은");
+                    // 이미지 뷰 결과화면 변경
+                    Drawable drawable = getResources().getDrawable(R.drawable.couple_entj);
+                    imageView_mbti_result.setImageDrawable(drawable);
+
+                }
+
+
 
             }
         });
